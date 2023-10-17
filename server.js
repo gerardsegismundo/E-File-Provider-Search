73ced578bbfba9249ffa8da7e849ee3a737049fa
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-16 18:25:18 -0700

    fix path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'path'
 import scrapeRoute from './routes/scrape.js'
 
 // Define __dirname using ES modules
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 const app = express()
 app.use(cors())
@@ -14,9 +14,9 @@ app.use(express.json())
 app.use('/api/scrape', scrapeRoute)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build'));
+  app.use(express.static('client/build'))
 
-  app.get('*', (_req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  app.get('*', (_req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')))
 }
 
 const PORT = process.env.PORT || 5000
