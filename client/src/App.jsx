5c9fa835554c3e35c58a2059f4265b7e1f33a156
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-16 23:30:02 -0700

    update for deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,25 @@
 import IRSLocator from './components/IRSLocator'
 import IRSTableGroup from './components/IRSTableGroup'
 import SortBox from './components/SortBox'
-import { Divider } from 'antd'
+import { Divider, Flex, Layout } from 'antd'
+
+const layoutStyle = {
+  margin: '7rem auto 0 auto',
+  padding: '0 5vw',
+  width: '100%',
+  backgroundColor: '#ffffff'
+}
 
 function App() {
   return (
-    <div className='container'>
-      <div className='header'>
+    <Layout style={layoutStyle}>
+      <Flex>
         <IRSLocator />
         <SortBox />
-      </div>
+      </Flex>
       <Divider style={{ marginBottom: '3rem' }} />
       <IRSTableGroup />
-    </div>
+    </Layout>
   )
 }
 
--- client/src/components/SortBox.jsx
+++ client/src/components/SortBox.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { sortBy } from '../redux/slice/IRSTableSlice'
 
-import { Flex, Select } from 'antd'
+import { Flex, Select, Form } from 'antd'
 const { Option } = Select
 
 const SortBox = () => {
@@ -16,22 +16,21 @@ const SortBox = () => {
   }
 
   return (
-    <Flex align='self-end' style={{ marginLeft: 'auto' }}>
-      <label htmlFor='SortBy' style={{ fontWeight: 600, marginBottom: '0.5rem', marginRight: '0.5rem' }}>
-        SortBy:
-      </label>
-      <Select
-        id='SortBy'
-        value={sortByOption}
-        onChange={handleSortChange}
-        style={{ width: 160 }}
-        disabled={IRSProviders.length <= 0}
-      >
-        <Option value='NameOfBusiness'>Name Of Business</Option>
-        <Option value='Address'>Address</Option>
-        <Option value='CityStateZIP'>City/State/ZIP</Option>
-        <Option value='PointOfContact'>Point of Contact</Option>
-      </Select>
+    <Flex align='self-end' style={{ marginLeft: 'auto', marginTop: '5rem' }}>
+      <Form.Item label='SortBy'>
+        <Select
+          id='SortBy'
+          value={sortByOption}
+          onChange={handleSortChange}
+          style={{ width: 160 }}
+          disabled={IRSProviders.length <= 0}
+        >
+          <Option value='NameOfBusiness'>Name Of Business</Option>
+          <Option value='Address'>Address</Option>
+          <Option value='CityStateZIP'>City/State/ZIP</Option>
+          <Option value='PointOfContact'>Point of Contact</Option>
+        </Select>
+      </Form.Item>
     </Flex>
   )
 }
